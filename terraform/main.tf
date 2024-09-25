6a98ca4bf947b2cc097412525b12fd5dbd53bfcb
Aleksandar.Grcic@contracted.pmi.com <Aleksandar.Grcic@contracted.pmi.com>
2024-09-25 11:20:14 +0200

    Updated tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,12 +11,46 @@ resource "aws_sqs_queue" "sqs-queue-1" {
 
 
 # EVENT BRIDGE
+# Create Role for EventBridge
+resource "aws_iam_role" "eventbridge_role" {
+  name = "agrcic-eventbridge-role-1-${var.part}"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "events.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+# Create Polivy for EventBridge
+resource "aws_iam_role_policy" "eventbridge_policy" {
+  name   = "agrcic-eventbridge-policy-1-${var.part}"
+  role   = aws_iam_role.eventbridge_role.id
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": "sqs:SendMessage",
+        "Resource": aws_sqs_queue.sqs-queue-1.arn
+      }
+    ]
+  })
+}
+
+
 # Create EventBridge Rule
 resource "aws_cloudwatch_event_rule" "eb-rule-1" {
   name = "agrcic-eb-rule-1-${var.part}"
   event_pattern = jsonencode({
     source = ["demo.sqs"]
-#     detail-type = ["agrcic-detail-type-1-${var.part}"]
   })
   depends_on = [aws_sqs_queue.sqs-queue-1]
 }
@@ -26,6 +60,7 @@ resource "aws_cloudwatch_event_target" "eb-target-1" {
   arn  = aws_sqs_queue.sqs-queue-1.arn
   depends_on = [aws_cloudwatch_event_rule.eb-rule-1]
   target_id = "agrcic-target-1-${var.part}"
+  role_arn = aws_iam_role.eventbridge_role.arn
 }
 # Grant EventBridge Permissions to Send Messages to SQS
 resource "aws_sqs_queue_policy" "event_queue_policy" {
