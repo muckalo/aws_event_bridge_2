87eb11b3b1520711d69c94760203880f22a3f809
Aleksandar.Grcic@contracted.pmi.com <Aleksandar.Grcic@contracted.pmi.com>
2024-09-25 12:44:00 +0200

    Updated tf and added lambda_3

--- terraform/main.tf
+++ terraform/main.tf
@@ -134,7 +134,7 @@ resource "aws_iam_role_policy" "eventbridge_policy_3" {
 
 
 # Lambda
-resource "aws_iam_role" "lambda_role-1" {
+resource "aws_iam_role" "lambda_role_1" {
   name = "agrcic-lambda-role-1-${var.part}"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -154,7 +154,7 @@ resource "aws_lambda_function" "agrcic-lambda-1" {
   function_name = "agrcic-lambda-1-${var.part}"
   handler = "lambda_1.lambda_handler"
   runtime = "python3.9"
-  role = aws_iam_role.lambda_role-1.arn
+  role = aws_iam_role.lambda_role_1.arn
   source_code_hash = filebase64sha256("../lambda_functions.zip")
   filename = "../lambda_functions.zip"
 }
@@ -162,7 +162,7 @@ resource "aws_lambda_function" "agrcic-lambda-2" {
   function_name = "agrcic-lambda-2-${var.part}"
   handler = "lambda_2.lambda_handler"
   runtime = "python3.9"
-  role = aws_iam_role.lambda_role-1.arn
+  role = aws_iam_role.lambda_role_1.arn
   source_code_hash = filebase64sha256("../lambda_functions.zip")
   filename = "../lambda_functions.zip"
 }
@@ -170,7 +170,7 @@ resource "aws_lambda_function" "agrcic-lambda-3" {
   function_name = "agrcic-lambda-3-${var.part}"
   handler = "lambda_3.lambda_handler"
   runtime = "python3.9"
-  role = aws_iam_role.lambda_role-1.arn
+  role = aws_iam_role.lambda_role_1.arn
   source_code_hash = filebase64sha256("../lambda_functions.zip")
   filename = "../lambda_functions.zip"
 }
