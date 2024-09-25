f950fd2c3ca2cd2ee487f91802b20faf3e182978
Aleksandar.Grcic@contracted.pmi.com <Aleksandar.Grcic@contracted.pmi.com>
2024-09-25 10:36:18 +0200

    Updated tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,17 +6,17 @@ provider "aws" {
 
 # SQS
 resource "aws_sqs_queue" "sqs-queue-1" {
-  name = "agrcic-sqs-queue-${var.part}"
+  name = "agrcic-sqs-queue-1-${var.part}"
 }
 
 
 # EVENT BRIDGE
 # Create EventBridge Rule
 resource "aws_cloudwatch_event_rule" "eb-rule-1" {
-  name = "agrcic-eb-rule-${var.part}"
+  name = "agrcic-eb-rule-1-${var.part}"
   event_pattern = jsonencode({
     source = ["com.myapp.sqs"]
-    detail-type = ["agrcic-detail-type-${var.part}"]
+    detail-type = ["agrcic-detail-type-1-${var.part}"]
   })
   depends_on = [aws_sqs_queue.sqs-queue-1]
 }
@@ -25,7 +25,7 @@ resource "aws_cloudwatch_event_target" "eb-target-1" {
   arn  = aws_sqs_queue.sqs-queue-1.arn
   rule = aws_cloudwatch_event_rule.eb-rule-1.name
   depends_on = [aws_cloudwatch_event_rule.eb-rule-1]
-  target_id = "agrcic-target-${var.part}"
+  target_id = "agrcic-target-1-${var.part}"
 }
 # Grant EventBridge Permissions to Send Messages to SQS
 resource "aws_sqs_queue_policy" "event_queue_policy" {
@@ -35,7 +35,7 @@ resource "aws_sqs_queue_policy" "event_queue_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid = "agrcic-EventBridgeSendMessage-${var.part}",
+        Sid = "agrcic-EventBridgeSendMessage-1-${var.part}",
         Effect = "Allow"
         Principal = {
           Service = "events.amazonaws.com"
@@ -57,12 +57,12 @@ resource "aws_sqs_queue_policy" "event_queue_policy" {
 
 # Lambda
 # resource "aws_iam_role" "lambda_role-1" {
-#   name = "agrcic-lambda-role-${var.part}"
+#   name = "agrcic-lambda-role-1-${var.part}"
 #   assume_role_policy = jsonencode({
 #     Version = "2012-10-17"
 #     Statement = [
 #       {
-#         Sid = "agrcic-lambda-policy-${var.part}"
+#         Sid = "agrcic-lambda-policy-1-${var.part}"
 #         Effect = "Allow"
 #         Action = "sts:AssumeRole"
 #         Principal = {
@@ -73,7 +73,7 @@ resource "aws_sqs_queue_policy" "event_queue_policy" {
 #   })
 # }
 # resource "aws_lambda_function" "agrcic-lambda-1" {
-#   function_name = "agrcic-lambda-1"
+#   function_name = "agrcic-lambda-1-${var.part}"
 #   handler = "lambda_1.lambda_handler"
 #   runtime = "python3.9"
 #   role = aws_iam_role.lambda_role-1.arn
@@ -81,7 +81,7 @@ resource "aws_sqs_queue_policy" "event_queue_policy" {
 #   filename = "../lambda_functions.zip"
 # }
 # resource "aws_lambda_function" "agrcic-lambda-2" {
-#   function_name = "agrcic-lambda-2"
+#   function_name = "agrcic-lambda-2-${var.part}"
 #   handler = "lambda_2.lambda_handler"
 #   runtime = "python3.9"
 #   role = aws_iam_role.lambda_role-1.arn
